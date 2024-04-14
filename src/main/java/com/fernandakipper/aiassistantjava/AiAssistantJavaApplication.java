b1a7ab08a24e0bfde2fa2c1fe71538e4ff3a0b0c
Fernanda <nanda.kipper@gmail.com>
2024-04-14 19:18:47 -0300

    feat: first commit

---
+++ src/main/java/com/fernandakipper/aiassistantjava/AiAssistantJavaApplication.java
@@ -0,0 +1,13 @@
+package com.fernandakipper.aiassistantjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAssistantJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiAssistantJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernandakipper/aiassistantjava/AiAssistantJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandakipper.aiassistantjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAssistantJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
